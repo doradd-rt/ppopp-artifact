87f097cf1b40f9d6f1199d84e3b58db12ee96ce8
Scofield626 <scofield_zliu@outlook.com>
2024-11-21 13:56:57 +0000

    update caracal run script

--- caracal/run_ycsb.sh
+++ caracal/run_ycsb.sh
@@ -1,13 +1,15 @@
 pushd felis
+git checkout ycsb
 git submodule update --init
 buck build db_release
 popd
 
 pushd felis-controller
+git checkout ycsb
 sudo rm -rf ./out # clean build
 ./mill FelisController.assembly
 popd
 
 pushd felis-controller/scripts
-./expr-workflow.sh
+./run_ycsb.sh
 popd
